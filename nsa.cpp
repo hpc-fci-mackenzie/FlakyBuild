1e9e1ac29f3e60a68446d3c6c50d6c3beb338ad6
Calebe de Paula Bianchini <prof.calebe.bianchini@gmail.com>
2020-04-15 23:03:57 -0300

    Change the input format: from hard-coding to config file

--- nsa.cpp
+++ nsa.cpp
@@ -8,26 +8,58 @@
 
 typedef double datatype;
 
-constexpr int problem_size = 44;
-datatype search_space[problem_size][problem_size];
+struct {
+    int problem_size;
+    int max_detectors;
+    datatype min_dist;
+    int amount_of_proofs;
+    char trainning_dataset_csv_file[100] = {0};
+    char testing_dataset_csv_file[100] = {0};
+    std::vector<int> expected_detected;
+    datatype *search_space;
+} config;
+
+void read_config_file(const char* config_file)
+{
+    csv::CSVFormat format;
+    format.trim({ ' ', '\t'  });
+    format.variable_columns(csv::VariableColumnPolicy::KEEP);
+    csv::CSVReader reader(config_file, format);
+    csv::CSVRow row;
+    // 1st row
+    reader.read_row(row);
+    config.problem_size = row[0].get<int>();
+    config.max_detectors = row[1].get<int>();
+    config.min_dist = row[2].get<datatype>();
+    config.amount_of_proofs = row[3].get<int>();
+    row[4].get().copy(config.trainning_dataset_csv_file, 100);
+    row[5].get().copy(config.testing_dataset_csv_file, 100);
+    // 2nd row
+    reader.read_row(row);
+    for (csv::CSVField& field: row) {
+        config.expected_detected.push_back(field.get<int>());
+    }
+}
 
 void init_search_space(void)
 {
-    for (int i = 0; i < problem_size; i++) {
-        search_space[i][0] = 0.0;
-        search_space[i][1] = 1.0;
+    config.search_space = new datatype[config.problem_size * 2];
+    for (int i = 0; i < config.problem_size; i++) {
+        config.search_space[2 * i] = 0.0;
+        config.search_space[2 * i + 1] = 1.0;
     }
 }
 
 std::vector<datatype *> *read_dataset(const char *filename)
 {
     std::vector<datatype *> *data = new std::vector<datatype *>();
-    std::vector<std::string> none(problem_size, "none");
+    std::vector<std::string> none(config.problem_size, "none");
     csv::CSVFormat format;
+    format.trim({ ' ', '\t'  });
     format.column_names(none);
     csv::CSVReader reader(filename, format);
     for (csv::CSVRow &row : reader) {
-        datatype *row_data = new datatype[problem_size];
+        datatype *row_data = new datatype[config.problem_size];
         int row_count = 0;
         for (csv::CSVField &field : row) {
             row_data[row_count] = field.get<datatype>();
@@ -55,28 +87,29 @@ double Random(void)
  * ----------------------------------------------------------------
  */
 {
-  const long Q = MODULUS / MULTIPLIER;
-  const long R = MODULUS % MULTIPLIER;
-        long t;
+    const long Q = MODULUS / MULTIPLIER;
+    const long R = MODULUS % MULTIPLIER;
+    long t;
 
-  t = MULTIPLIER * (seed % Q) - R * (seed / Q);
-  if (t > 0)
-    seed = t;
-  else
-    seed = t + MODULUS;
-  return ((double) seed / MODULUS);
+    t = MULTIPLIER * (seed % Q) - R * (seed / Q);
+    if (t > 0) {
+        seed = t;
+    } else {
+        seed = t + MODULUS;
+    }
+    return ((double) seed / MODULUS);
 }
 
 void random_vector(datatype *vector)
 {
-    for (int i = 0; i < problem_size; i++) {
-        vector[i] = search_space[i][0] + ((search_space[i][1] - search_space[i][0]) * Random());
+    for (int i = 0; i < config.problem_size; i++) {
+        vector[i] = config.search_space[2 * i] + ((config.search_space[2 * i + 1] - config.search_space[2 * i]) * Random());
     }
 }
 
 datatype euclidean_distance(datatype *vector, datatype *points) {
     double sum = 0;
-    for (int i = 0; i < problem_size; i++) {
+    for (int i = 0; i < config.problem_size; i++) {
         double tmp = vector[i] - points[i];
         sum += (tmp * tmp);
     }
@@ -94,43 +127,41 @@ bool matches(datatype *vector, std::vector<datatype *> *dataset, datatype min_di
     return false;
 }
 
-std::vector<datatype*> *generate_detectors(int max_detectors, std::vector<datatype *> *self_dataset, datatype min_dist, int generation)
+std::vector<datatype*> *generate_detectors(std::vector<datatype *> *self_dataset, int generation)
 {
     std::vector<datatype *> *detectors = new std::vector<datatype *>();
     std::cout << "Generating detectors..." << std::endl;
-    datatype *detector = new datatype[problem_size];
+    datatype *detector = new datatype[config.problem_size];
     int count = 1;
     do {
         random_vector(detector);
-        if (!matches(detector, self_dataset, min_dist)) {
+        if (!matches(detector, self_dataset, config.min_dist)) {
             if (!matches(detector, detectors, 0.0)) {
                 detectors->push_back(detector);
-                detector = new datatype[problem_size];
+                detector = new datatype[config.problem_size];
             }
-            std::cout << count++ << "/" << max_detectors << std::endl;
+            std::cout << count++ << "/" << config.max_detectors << std::endl;
         }
-    } while (detectors->size() < max_detectors);
+    } while (detectors->size() < config.max_detectors);
     if (detector != *detectors->cend()) {
         delete[] detector;
     }
     return detectors;
 }
 
-std::string apply_detectors(std::vector<datatype*> *detectors, std::vector<datatype *> *self_dataset, datatype min_dist)
+std::string apply_detectors(std::vector<datatype*> *detectors, std::vector<datatype *> *self_dataset)
 {
     std::set<int> *detected = new std::set<int>();
     int trial = 1;
     for (auto it = self_dataset->cbegin(); it != self_dataset->cend(); it++) {
-        bool actual = matches(*it, detectors, min_dist);
-        bool expected = matches(*it, self_dataset, min_dist);
+        bool actual = matches(*it, detectors, config.min_dist);
+        bool expected = matches(*it, self_dataset, config.min_dist);
         if (actual == expected) {
             detected->insert(trial);
         }
         trial++;
     }
-    int values[] = {11, 15, 23, 24, 25, 30, 37, 55, 72};
-
-    std::set<int> expected_detected(std::begin(values), std::end(values));
+    std::set<int> expected_detected(config.expected_detected.begin(), config.expected_detected.end());
     datatype expected_detected_size = expected_detected.size();
     for (auto it = detected->cbegin(); it != detected->cend(); it++) {
         auto found = expected_detected.find(*it);
@@ -142,7 +173,7 @@ std::string apply_detectors(std::vector<datatype*> *detectors, std::vector<datat
     std::string results = std::to_string(-(new_expected_detected_size - expected_detected_size) / expected_detected_size);
 
     expected_detected.clear();
-    expected_detected.insert(std::begin(values), std::end(values));
+    expected_detected.insert(config.expected_detected.begin(), config.expected_detected.end());
     for (auto it = expected_detected.cbegin(); it != expected_detected.cend(); it++) {
         auto found = detected->find(*it);
         if (found != detected->end()) {
@@ -155,45 +186,37 @@ std::string apply_detectors(std::vector<datatype*> *detectors, std::vector<datat
     return results + "\n";
 }
 
-void run(int max_detectors, datatype min_dist, int amount_of_proofs)
+void run()
 {
     std::string general_results("");
 
     init_search_space();
 
-    for (int proof = 0; proof < amount_of_proofs; proof++) {
-        std::vector<datatype*>* self_dataset_for_training = read_dataset("dataset_trainning.csv");
-        std::vector<datatype*>* generate_self_dataset_for_testing = read_dataset("dataset_testing.csv");
-        std::vector<datatype*> *detectors = generate_detectors(max_detectors, self_dataset_for_training, min_dist, proof + 1);
-        general_results.append(apply_detectors(detectors, generate_self_dataset_for_testing, min_dist));
+    std::vector<datatype*>* self_dataset_for_training = read_dataset(config.trainning_dataset_csv_file);
+    std::vector<datatype*>* generate_self_dataset_for_testing = read_dataset(config.testing_dataset_csv_file);
+
+    for (int proof = 0; proof < config.amount_of_proofs; proof++) {
+        std::vector<datatype*> *detectors = generate_detectors(self_dataset_for_training, proof + 1);
+        general_results.append(apply_detectors(detectors, generate_self_dataset_for_testing));
     }
 
-    std::cout << "Detectors: " << max_detectors << std::endl;
-    std::cout << "Min. distance: " << min_dist << std::endl;
-    std::cout << "Runs: " << amount_of_proofs << std::endl;
+    std::cout << "Detectors: " << config.max_detectors << std::endl;
+    std::cout << "Min. distance: " << config.min_dist << std::endl;
+    std::cout << "Runs: " << config.amount_of_proofs << std::endl;
     std::cout << general_results << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
-    int max_detectors, amount_of_proofs;
-    datatype min_dist;
-
-    if (argc == 4) {
-        max_detectors = std::atoi(argv[1]);
-        min_dist = std::atof(argv[2]);
-        amount_of_proofs = std::atoi(argv[3]);
-    } else if (argc == 1) {
-        max_detectors = 1000;
-        min_dist = 3;
-        amount_of_proofs = 1;
+    if (argc == 2) {
+        read_config_file(argv[1]);
     } else {
-        std::cout << "Usage: " << argv[0] << " [MAX-DETECTORS] [MIN-DISTANCE] [AMOUNT-OF-POOFS]" << std::endl
+        std::cout << "Usage: " << argv[0] << " <CONFIG-FILE>" << std::endl
                   << std::endl;
         return -1;
     }
 
-    run(max_detectors, min_dist, amount_of_proofs);
+    run();
 
     return 0;
 }
